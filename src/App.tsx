4f05046663485ce2d269704aa9366d35c7f04685
Rafael Ferreira <andersonrafaelferreira@gmail.com>
2024-11-22 13:21:06 +0000

    styled added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import { useState, useRef } from "react";
 import "./App.css";
 
+import styled from 'styled-components'
+
+const DateContainer = styled.div`
+  width: 100%;
+  padding: 12px 16px;
+  border-radius: .5rem;
+  border: 1px solid #d9d9d9;
+  height: 24px;
+`
+
 function App() {
   const [selectedDate, setSelectedDate] = useState("");
   const inputRef = useRef<HTMLInputElement | null>(null);
@@ -16,16 +26,18 @@ function App() {
   };
 
   return (
-    <>
+    <DateContainer>
       <input
         ref={inputRef}
         type="date"
         value={selectedDate}
         onChange={handleDateChange}
-        style={{visibility: 'hidden'}}
+        style={{opacity: 0}}
       />
       <button onClick={clickCalendarIcon}>Open Calendar</button>
-    </>
+
+      <input type="text" value={selectedDate} />
+    </DateContainer>
   );
 }
 
