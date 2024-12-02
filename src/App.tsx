c0606c119137052822271eac56268a9b44349e2a
Rafael Ferreira <andersonrafaelferreira@gmail.com>
2024-12-02 14:28:31 -0300

    fix the initial date

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,19 @@
 import { useState, useRef, useEffect } from "react";
 import "./App.css";
 
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const DateContainer = styled.div`
   max-width: 100%;
   padding: 16px;
-  border-radius: .5rem;
+  border-radius: 0.5rem;
   border: 1px solid #d9d9d9;
   height: 24px;
-
-`
-
+`;
 
 function App() {
   const [selectedDate, setSelectedDate] = useState("");
   const inputRef = useRef<HTMLInputElement | null>(null);
-
-  useEffect(() => { 
-    const hoje = new Date(); 
-    const dia = String(hoje.getDate()).padStart(2, '0'); 
-    const mes = String(hoje.getMonth() + 1).padStart(2, '0'); 
-    const ano = hoje.getFullYear(); 
-    const dataFormatada = `${ano}/${mes}/${dia}`; 
-    setSelectedDate(dataFormatada); 
-  }, []);
 
   const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedDate(event.target.value);
@@ -37,36 +26,63 @@ function App() {
   };
 
   function formatDate(date: string): string {
-    const [year, month, day] = date.split('-');
-    const formattedDate = `${day}/${month}/${year}`; 
+    const [year, month, day] = date.split("-");
+    const formattedDate = `${day}/${month}/${year}`;
     return formattedDate;
   }
+
+  useEffect(() => {
+    const hoje = new Date();
+    const dia = String(hoje.getDate()).padStart(2, "0");
+    const mes = String(hoje.getMonth() + 1).padStart(2, "0");
+    const ano = hoje.getFullYear();
+    const dataFormatada = `${ano}-${mes}-${dia}`;
+    setSelectedDate(dataFormatada);
+  }, []);
 
   return (
     <>
       <DateContainer onClick={clickCalendarIcon}>
-        <div style={{
-          flexDirection: 'row', gap: '8px', display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center'
-        }}>
+        <div
+          style={{
+            flexDirection: "row",
+            gap: "8px",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
           {/* <input type="date" value={selectedDate} disabled 
           style={{
             height: '24px', border: 0,
             background: 'transparent', display: 'flex', flex: 1, fontSize: '20px', textAlign: 'left'
           }} /> */}
-          <span>{selectedDate !== 'undefined' && formatDate(selectedDate)}</span>
+          <span>
+            {selectedDate && formatDate(selectedDate)}
+          </span>
           <input
-          ref={inputRef}
-          type="date"
-          value={selectedDate}
-          onChange={handleDateChange}
-          style={{ opacity: 0, height: '24px', display: 'flex', flex: 1, border: 0 }}
-        />
-          <img src="https://cdn-icons-png.flaticon.com/128/2948/2948088.png" width={16} height={16} />
+            ref={inputRef}
+            type="date"
+            value={selectedDate}
+            onChange={handleDateChange}
+            style={{
+              opacity: 0,
+              height: "24px",
+              display: "flex",
+              flex: 1,
+              border: 0,
+            }}
+          />
+          <button>
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/2948/2948088.png"
+              width={16}
+              height={16}
+            />
+          </button>
         </div>
       </DateContainer>
-      <p>{selectedDate !== 'undefined' && formatDate(selectedDate)}</p>
+      <p>{selectedDate && formatDate(selectedDate)}</p>
     </>
   );
 }
